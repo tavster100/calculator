9d3fc92d0d615815df88ff75b137ccec0266e211
Octavian Popovici <octavianpopovici13@gmail.com>
2020-10-06 21:33:34 +0300

    importing new libs

--- new-React/src/App.js
+++ new-React/src/App.js
@@ -1,35 +1,29 @@
-const Pet = ({ name, animal, breed } /*props*/) => {
-  //destructuring {name,animal,breed} instead of props
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name /*props.name*/), //instead of props.name / use just name
-    React.createElement("h2", {}, animal /* props.animal */), //instead of props.animal / use just animal
-    React.createElement("h2", {}, breed /* props.breed */), //instead of props.breed / use just breed
-  ]);
-};
-
+import React from "react";
+import ReactDOM from "react-dom"; //IMPORTING reactDOM and react
+import {
+    Pet
+} from "./Pet";
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Lara",
-        animal: "Pisica",
-        breed: "Corcitura",
-      }),
-      React.createElement(Pet, {
-        name: "Aquaman",
-        animal: "Caine",
-        breed: "Corcitura",
-      }),
-      React.createElement(Pet, {
-        name: "Nemo",
-        animal: "Peste",
-        breed: "Somon",
-      }),
-    ] //children
-  );
+    return React.createElement(
+        "div", {}, [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Lara",
+                animal: "Pisica",
+                breed: "Corcitura",
+            }),
+            React.createElement(Pet, {
+                name: "Aquaman",
+                animal: "Caine",
+                breed: "Corcitura",
+            }),
+            React.createElement(Pet, {
+                name: "Nemo",
+                animal: "Peste",
+                breed: "Somon",
+            }),
+        ] //children
+    );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
